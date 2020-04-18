283f92e74f90bf3191ed5e92dff66a788a84e1cf
ayang818 <ayang818@qq.com>
2020-04-18 20:56:56 +0800

    (dev)完成短链接的重定向

--- src/routes/caster.ts
+++ src/routes/caster.ts
@@ -6,19 +6,23 @@ import { nToBase62 } from 'utils/base62util';
 import { settings } from 'settings';
 
 interface l2sBody {
-    longerLink: string,
+    longLink: string,
 }
 
 interface resBody {
-    shorterLink: string,
+    shortLink: string,
     message: string,
 }
 
-const ifExist = 'select * from kugga_links where longer_link = ?'
-const setStep = 'set session auto_increment_increment = 10;';
-const updateId = 'insert into kugga_id_generator() values ()';
-const getLastInsertId = 'select last_insert_id() as id';
-const insertRecord = 'insert into kugga_links(longer_link, shorter_link) values(?, ?)'
+const ifExistSQL = 'select * from kugga_links where longer_link = ?'
+
+const setStepSQL = 'set session auto_increment_increment = 10;';
+
+const updateIdSQL = 'insert into kugga_id_generator() values ()';
+
+const getLastInsertIdSQL = 'select last_insert_id() as id';
+
+const insertRecordSQL = 'insert into kugga_links(longer_link, shorter_link) values(?, ?)'
 
 /**
  *  POST body { longerLink: ''}
@@ -28,33 +32,33 @@ const insertRecord = 'insert into kugga_links(longer_link, shorter_link) values(
  */
 export const l2s = (request: Express.Request, response: Express.Response) => {
     const body: l2sBody = request.body;
-    let resbody: resBody = { shorterLink: "", message: "" };
+    let resbody: resBody = { shortLink: "", message: "" };
     mysqlClient.pool.getConnection((err, conn) => {
         // 先查数据库，是否已经有了这条长链接的映射
-        conn.query(ifExist, body.longerLink, (err, res) => {
+        conn.query(ifExistSQL, body.longLink, (err, res) => {
             if (err) throw err;
             // 已经有了，则直接返回短链接
             if (res.length != 0) {
-                resbody = { shorterLink: settings.SERVER_HOST + res[0].shorter_link, message: "success" };
+                resbody = { shortLink: settings.SERVER_HOST + res[0].shorter_link, message: "success" };
                 response.send(resbody);
             } else {
                 // 没有这条长链接映射，增加
                 conn.beginTransaction((err) => {
                     if (err) throw err;
                     // 设置步长
-                    conn.query(setStep);
+                    conn.query(setStepSQL);
                     // 更新Id
-                    conn.query(updateId)
+                    conn.query(updateIdSQL)
                     // 获取Id
-                    conn.query(getLastInsertId, (err, res, fileds) => {
+                    conn.query(getLastInsertIdSQL, (err, res, fileds) => {
                         if (err) throw err;
                         const curId = res[0].id;
                         // 转化为62进制
                         const suffix = nToBase62(curId);
                         // 插入链接映射表
-                        conn.query(insertRecord, [body.longerLink, suffix], (err, res) => {
+                        conn.query(insertRecordSQL, [body.longLink, suffix], (err, res) => {
                             if (err) throw err;
-                            resbody = { shorterLink: settings.SERVER_HOST + suffix, message: "success" };
+                            resbody = { shortLink: settings.SERVER_HOST + suffix, message: "success" };
                             response.send(resbody);
                         });
                     })
--- src/routes/redirector.ts
+++ src/routes/redirector.ts
@@ -1,10 +1,35 @@
 import * as Express from 'express';
+import { mysqlClient } from 'utils/mysqlClient';
+import { log } from 'app';
+
+const selectOriginLinkSql = 'select longer_link from kugga_links where shorter_link = ?'
+
+interface LongLink {
+    longer_link: string
+}
 
 /**
  * @desc 为储存了的短链接提供重定向到源地址
- * @param req 
- * @param res 
+ * @param request 
+ * @param response 
  */
-export const redirect = (req: Express.Request, res: Express.Response) => {
-    
+export const redirect = (request: Express.Request, response: Express.Response) => {
+    if (!request.params.base62url) {
+        response.send({'message': 'error'});
+        return;
+    }
+    mysqlClient.pool.getConnection((err, conn) => {
+        if (err) throw err;
+        conn.query(selectOriginLinkSql, [request.params.base62url], (err, res: Array<LongLink>) => {
+            if (res.length == 0) {
+                response.send({'message': 'no such link'});
+                return ;
+            } else {
+                response.status(302);
+                response.setHeader("Location", res[0].longer_link);
+                response.send();
+                log.info(`rediect id : ${request.params.base62url} to ${res[0].longer_link}`)               
+            }
+        })
+    })
 }
